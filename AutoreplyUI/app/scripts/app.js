34aeabc054bdacddd60a1aa87ad2995de3d9bd33
BennyImmanuel2001 <77180804+BennyImmanuel2001@users.noreply.github.com>
2021-05-20 11:43:23 +0530

    Update app.js

--- AutoreplyUI/app/scripts/app.js
+++ AutoreplyUI/app/scripts/app.js
@@ -8,20 +8,58 @@ document.onreadystatechange = function () {
 
     function getClient(_client) {
       window.client = _client;
-      client.events.on('app.activated', onAppActivate);
-    }
-  }
-};
-
-function onAppActivate() {
-  var textElement = document.getElementById('apptext');
+      var textElement = document.getElementById('apptext');
   var getContact = client.data.get('contact');
-
-  client.interface.trigger("showModal", {
-    title: 'Create a Scheduled Reply',
-    template: "view/modal.html",
-    data: {}
+ 
+  client.data.get("ticket").then(function(data) {
+    console.log("ticket >>>>>>>>>" , data)
+  },function (err){
+    console.log(err)
+    console.log("ticket")
   });
+  client.data.get("contact").then (
+    function(data) {
+        console.log("contact")
+        console.log(data.contact)
+      
+    },
+    function(error) {
+        console.log(error)
+  });  
+  
+  Promise.all([client.data.get("ticket"),client.data.get('contact')]).then(res=>
+    {
+      console.log(res)
+
+      client.interface.trigger("showModal", {
+        title: 'Create a Scheduled Reply',
+        template: "view/modal.html",
+        data: {}
+      });
+      
+    }).catch(er=>
+    {
+      console.clear()
+      console.log("from app in promise")
+      console.log(er)
+    })
+    // client.data.get("ticket").then (function(data) {
+    //   let eventID = `Reply_${ data.ticket.id}`;
+    //   console.log(eventID)
+    // },function (err){
+    //   console.log(err)
+    //   console.log("ticket")
+    // });
+
+
+
+    client.interface.trigger("showModal", {
+      title: 'Create a Scheduled Reply',
+      template: "view/modal.html",
+      data: {}
+    });
+
+
   client.instance.receive(
     function(event)  {
       var data = event.helper.getData();
@@ -32,7 +70,10 @@ function onAppActivate() {
   function showContact(payload) {
     textElement.innerHTML = `Ticket created by ${payload.contact.name}`;
   }
-}
+    }
+  }
+};
+
 
 
 function notifyMe(m_type,msg)
